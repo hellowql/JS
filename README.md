f0f1d8e44dc107b2295bd93d4e1315271321df28
wuqinglong <hellowql@126.com>
2013-04-15 11:32:35 -0700

    add a frame for js

---
+++ README.md
@@ -0,0 +1,4 @@
+JS
+==
+
+my js code
---
+++ js/event.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: WUQL
+ * Date: 13-4-16
+ * Time: 上午2:42
+ * To change this template use File | Settings | File Templates.
+ */
