ca525b3e4778f41fd5338161f39d381de2edc59f
wuqinglong <hellowql@126.com>
2013-04-23 17:02:17 +0800

    modify trundle lib

--- js/trundle/jquery-trundle.js
+++ js/trundle/jquery-trundle.js
@@ -14,14 +14,50 @@
  */
 if (!!window.jQuery) {
     (function ($, undefined) {
-        $.fn.trundle = function (obj) {
-            var param;
-            param = $.extend(true, {
+        function TRUNDLE($el, param) {
+            this.$el = $el;
+            this.length = $el.children().length;
+            this.index = 0;
+            this.param = param;
+        }
 
-            }, obj);
-
-
+        TRUNDLE.prototype.scroll = function () {
+            var _this = this;
+            var scrollTop = this.scrollTop();
+            this.$el.animate({
+                scrollTop:scrollTop
+            }, 500);
+            this.timer = setTimeout(function () {
+                _this.scroll();
+            }, this.param.time);
             return this;
+        }
+        TRUNDLE.prototype.ready = function () {
+            return true;
+        }
+        TRUNDLE.prototype.start = function () {
+            return this.ready() && this.scroll();
+        }
+        TRUNDLE.prototype.pause = function () {
+            return this;
+        }
+        TRUNDLE.prototype.continue = function () {
+            return this;
+        }
+        TRUNDLE.prototype.scrollTop = function () {
+            this.index += this.param.scroll;
+            this.index %= this.length;
+            return (this.index / this.param.scroll) * parseInt(this.param.height)
+        }
+        $.fn.trundle = function (obj) {
+            var param = $.extend(true, {
+                scroll:1,
+                time:3000,
+                height:'22px'
+            }, obj);
+            return this.each(function () {
+                new TRUNDLE($(this), param).start();
+            });
         }
     }(window.jQuery))
 }
