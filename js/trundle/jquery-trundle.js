abbd3a9cb52122f0407cdfce289e3acc30056d94
wuqinglong <hellowql@126.com>
2013-04-24 16:33:50 +0800

    modify trundle lib

--- js/trundle/jquery-trundle.js
+++ js/trundle/jquery-trundle.js
@@ -14,45 +14,133 @@
  */
 if (!!window.jQuery) {
     (function ($, undefined) {
+        var tool = {
+            height:function ($el) {
+                var height = 0;
+                $.each($el, function () {
+                    height += $(this).outerHeight();
+                })
+                return height;
+            },
+            min:function ($el) {
+                return Math.min.apply(null, $.map($el, function (n) {
+                    return $(n).outerHeight();
+                }));
+            },
+            getCloneEl:function (height, $els, type) {
+                var len = 1, $slice;
+                if (type == 0) {
+                    while (true) {
+                        $slice = $els.slice(0, len);
+                        if (tool.height($slice) / height >= 1) {
+                            console.log(height, type, len)
+                            break;
+                        }
+                        len++;
+                    }
+                } else {
+                    while (true) {
+                        $slice = $els.slice(-len);
+                        if (tool.height($slice) / height >= 1) {
+                            console.log(height, type, len)
+                            break;
+                        }
+                        len++;
+                    }
+                }
+                return $slice.clone().addClass('cloned');
+            }
+        };
+
         function TRUNDLE(el, param) {
             this.$el = el;
-            this.length = el.children().length;
-            this.index = 0;
             this.param = param;
         }
 
-        TRUNDLE.prototype.scroll = function () {
+        TRUNDLE.prototype.scroll = function (goAtOnce) {
             var _this = this;
-            this.$el.animate({
-                scrollTop:_this.scrollStep()
-            }, 500);
+            var pass = this.currenScrollTop + this.fixedHeight - this.heights[1];
+            if (pass > 0) {// 是否滚动完所有原始数据
+                this.currenScrollTop = pass;
+                this.$el.animate({
+                    scrollTop:_this.currenScrollTop
+                }, 0);
+            }
+            if (goAtOnce) {
+                this.$el.animate({
+                    scrollTop:_this.scrollStep()
+                }, 500);
+            }
+            this.lasttime = new Date().getTime();
             this.timer = setTimeout(function () {
-                _this.scroll();
+                _this.scroll(true);
             }, this.param.time);
             return this;
         };
         TRUNDLE.prototype.ready = function () {
-            return true;
+            return this.$el.children().length > this.param.scroll || tool.height(this.$el.children()) > this.param.height;
+        };
+        TRUNDLE.prototype.init = function () {
+            var _this = this;
+            this.$children = this.$el.children();
+            this.childlenth = this.$children.length;
+            this.fixedScroll = this.param.scroll;
+            this.fixedHeight = parseInt(this.param.height);
+            if (isNaN(this.fixedHeight) && this.fixedScroll > 0) {
+                this.fixedHeight = tool.min(this.$children) * this.fixedScroll;
+            }
+            if (this.fixedHeight > 0) {
+                this.fixedScroll = Math.round(this.fixedHeight / tool.min(this.$children));
+            }
+            this.currenScrollTop = this.fixedHeight;
+            this.$el
+                .prepend(tool.getCloneEl(this.fixedHeight, this.$children, 1))
+                .append(tool.getCloneEl(this.fixedHeight, this.$children, 0))
+                .animate({scrollTop:_this.currenScrollTop}, 0);
+            this.heights = [];
+            this.heights[0] = this.fixedHeight;
+            this.heights[1] = tool.height(this.$children) + this.heights[0];
+            this.heights[2] = this.fixedHeight + this.heights[1];
+
+
+            this.$el.hover(function () {
+                _this.pause();
+            }, function () {
+                _this.continue();
+            })
+            return this;
         };
         TRUNDLE.prototype.start = function () {
-            return this.ready() && this.scroll();
+            return this.ready() && this.init() && this.scroll();
         };
         TRUNDLE.prototype.pause = function () {
+            clearTimeout(this.timer);
+            this.$el.stop(true);
+            this.pausetime = new Date().getTime();
             return this;
         };
         TRUNDLE.prototype.continue = function () {
+            var _this = this;
+            var time = this.pausetime - this.lasttime;
+            if (!isNaN(time) && time > 0) {
+                if (this.$el.prop('scrollTop') != this.currenScrollTop) {
+                    this.$el.animate({
+                        scrollTop:_this.currenScrollTop
+                    }, time);
+                }
+                this.scroll();
+            }
             return this;
         };
         TRUNDLE.prototype.scrollStep = function () {
-            this.index += this.param.scroll;
-            this.index %= this.length;
-            return (this.index / this.param.scroll) * parseInt(this.param.height)
+            this.currenScrollTop += this.fixedHeight;
+            return this.currenScrollTop;
         };
         $.fn.trundle = function (obj) {
             var param = $.extend(true, {
-                scroll:1,
-                time:3000,
-                height:'22px'
+                scroll:3,
+                height:44,
+                time:3000
             }, obj);
             return this.each(function () {
                 new TRUNDLE($(this), param).start();
