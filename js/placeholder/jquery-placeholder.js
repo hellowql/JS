1d220cbaafce395a1162a2ab62496692fef08896
wuqinglong <hellowql@126.com>
2013-05-02 23:31:02 +0800

    modify placeholder,refacter,no need wrap el by span/div,can repeat call this lib or this fn

--- js/placeholder/jquery-placeholder.js
+++ js/placeholder/jquery-placeholder.js
@@ -12,36 +12,48 @@
  * Version: 0.1
  */
 if (!!window.jQuery) {
-    (function ($, undefined) {
-        if ($.fn.placeholder) {
+    (function ($, plugin, undefined) {
+        if ($.fn[plugin]) {
             return;
         }
-        $.fn.placeholder = function (obj) {
+        var id = 0;
+
+        function uuid() {
+            return id++;
+        }
+
+        $.fn[plugin] = function (obj) {
             var html, placeholder, param, supported;
             param = $.extend(true, {
                 placeholder: 'placeholder', //placeholder attribute,default will use explorer original function
                 css: {// placeholder text dom css
-                    position: 'absolute',
-                    top: '5px',
-                    left: '4px',
-                    color: '#bbb',
-                    cursor: 'text',
-                    fontSize: '12px'
+//                    position: 'absolute',
+//                    top: '5px',
+//                    left: '4px',
+//                    color: 'red'
+                    cursor: 'text'
+//                    fontSize: '12px'
                 },
-                hideCss: false, //className and css how to take effect,true just className take effect
+//                hideCss: false, //className and css how to take effect,true just className take effect
                 hideStyle: 'value', //enum:value,focus;hide placeholder when focus or value exists,focus is of no effect when placeholder is 'placeholder'
                 className: ''//placeholder text dom className
             }, obj);
             supported = param.placeholder in document.createElement('input');
             if (!supported) {
                 this.each(function () {
-                    var $this = $(this);
+                    var $this = $(this), repeat;
                     placeholder = $this.attr(param.placeholder);
-                    if ($this.data('__placeholder__') === true || placeholder == undefined || $.trim(placeholder) == '') {// placeholder-ed el never do again
+                    if (/*$this.data('__placeholder__') === true || */placeholder == undefined || $.trim(placeholder) == '') {// placeholder-ed el never do again
                         return;
                     }
-                    html = $('<label/>');
-                    html.text(placeholder);
+                    repeat = $this.prev().hasClass(plugin);
+                    if (!repeat) {
+                        html = $('<label/>');
+                        html.addClass(param.className ? (plugin + ' ' + param.className) : plugin);
+                        html.text(placeholder);
+                    } else {
+                        html = $this.prev();
+                    }
                     //                    if (!param.hideCss) {
                     //                        html.css(param.css);
                     //                    }
@@ -52,7 +64,7 @@ if (!!window.jQuery) {
                     //                        $this.wrap('<div></div>')
                     //                    }
                     //                    $this.parent().css('position', 'relative');
-                    html.css({
+                    html.css($.extend({
                         position: 'absolute',
                         color: '#bbb',
                         display: 'inline',
@@ -64,23 +76,32 @@ if (!!window.jQuery) {
                         textIndent: $this.css('textIndent'),
                         paddingLeft: $this.css('borderLeftWidth'),
                         paddingTop: $this.css('borderTopWidth'),
+//                        paddingTop: parseInt($this.css('borderTopWidth'),10)+parseInt($this.css('paddingTop'),10)+'px',
                         paddingRight: $this.css('borderRightWidth'),
                         paddingBottom: $this.css('borderBottomWidth'),
                         fontSize: $this.css('fontSize'),
                         fontFamily: $this.css('fontFamily'),
                         fontWeight: $this.css('fontWeight')
-                    });
-                    if ($this.css('lineHeight')) {
-                        console.log($this.css('lineHeight'))
+                    }, param.css));
+                    //buttons or other input element, line-height has no effect,so use height replaced
+                    if ($this.is('input') || $this.is('button')) {
+                        html.css('lineHeight', $this.css('height'));
+                    } else {
                         html.css('lineHeight', $this.css('lineHeight'));
                     }
-                    $this.before(html);
-                    html.bind('click.placeholder', function () {
-                        $(this).next().focus();
-                    });
+                    if (!this.id) {
+                        this.id = plugin + '_' + uuid();
+                    }
+                    html.attr('for', this.id);
+//                    html.bind('click.placeholder', function () {
+//                        $(this).next().focus();
+//                    });
+                    if (!repeat) {
+                        $this.before(html);
+                    }
                     switch (param.hideStyle) {
                         case 'focus':
-                            $this.bind('focus.placeholder keyup.placeholder',function () {
+                            $this.unbind('focus.placeholder keyup.placeholder blur.placeholder').bind('focus.placeholder keyup.placeholder',function () {
                                 $(this).prev().css('display', 'none');
                             }).bind('blur.placeholder',function () {
                                     if (this.value.length > 0) {
@@ -91,7 +112,7 @@ if (!!window.jQuery) {
                                 }).triggerHandler('blur.placeholder');
                             break;
                         default :
-                            $this.bind('keyup.placeholder blur.placeholder',function () {
+                            $this.unbind('keyup.placeholder blur.placeholder').bind('keyup.placeholder blur.placeholder',function () {
                                 if (this.value.length > 0) {
                                     $(this).prev().css('display', 'none');
                                 } else {
@@ -99,11 +120,11 @@ if (!!window.jQuery) {
                                 }
                             }).triggerHandler('blur.placeholder');
                     }
-                    $this.data('__placeholder__', true);// mark this el placeholder
+//                    $this.data('__placeholder__', true);// mark this el placeholder
                 });
             }
             supported = html = placeholder = param = undefined;
             return this;
         }
-    }(window.jQuery))
+    }(window.jQuery, 'placeholder'))
 }
